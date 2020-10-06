9bf240933874c03ab62948f0770611b37569731a
Jaden Balogh <jadenbalogh@gmail.com>
2020-10-06 09:52:55 -0700

    Add base unit functionality

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    private Vector2 targetPos;
+    private bool selected;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
