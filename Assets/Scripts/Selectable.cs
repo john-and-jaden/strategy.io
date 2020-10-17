a926bc379716c62c00b9eb0dd8838fec1af4358e
Jaden Balogh <jadenbalogh@gmail.com>
2020-10-17 13:49:25 -0700

    Implement selection system

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Selectable : MonoBehaviour
+{
+    public SpriteRenderer hoverIndicatorPrefab;
+    public SpriteRenderer selectIndicatorPrefab;
+
+    protected bool hovered;
+    protected bool selected;
+    protected SpriteRenderer hoverIndicator;
+    protected SpriteRenderer selectIndicator;
+
+    protected void SpawnIndicators()
+    {
+        hoverIndicator = Instantiate(hoverIndicatorPrefab);
+        selectIndicator = Instantiate(selectIndicatorPrefab);
+    }
+
+    protected void UpdateIndicators()
+    {
+        hoverIndicator.enabled = hovered;
+        selectIndicator.enabled = selected;
+    }
+
+    public void SetHovered(bool hovered)
+    {
+        this.hovered = hovered;
+    }
+
+    public void SetSelected(bool selected)
+    {
+        this.selected = selected;
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,49 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour, ISelectable
+public class Unit : Selectable
 {
     public float moveSpeed = 1f;
     public float selectDistance;
-
-    public bool Selected { get; set; }
 
     private Vector2 targetPos;
 
     void Start()
     {
+        SpawnIndicators();
         targetPos = transform.position;
-        Selected = true;
     }
 
     void Update()
     {
-        if (Selected)
-        {
-            // Change current target on right-click
-            if (Input.GetButtonDown("Fire2"))
-            {
-                targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            }
-        }
-        else
-        {
-            // Select this object on left-click
-            if (Input.GetButtonDown("Fire1"))
-            {
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                float clickDist = Vector2.Distance(transform.position, mousePos);
-                if (clickDist < selectDistance)
-                {
-                    Selected = true;
-                }
-                else
-                {
-                    Selected = false;
-                }
-            }
-        }
+        // Update indicators
+        hoverIndicator.transform.position = transform.position;
+        selectIndicator.transform.position = transform.position;
+        UpdateIndicators();
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    public void SetTargetPos(Vector2 targetPos)
+    {
+        this.targetPos = targetPos;
     }
 }
