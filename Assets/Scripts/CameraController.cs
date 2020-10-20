61470464a5f6650bd11336a91659750989b27544
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-10-20 20:06:17 +0200

    Implement side movement when scrolling

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float zoomSpeed = 1f;
-    public float minZoom = 2f;
-    public float maxZoom = 20f;
+    public float zoomSpeed = 0.05f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float sideMoveSpeed = 0.025f;
 
     void Update()
     {
+        // Scrolling
         float scrollDir = -Input.mouseScrollDelta.y;
-        Camera.main.orthographicSize += scrollDir * zoomSpeed * Time.deltaTime;
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize + scrollDir * zoomSpeed * Time.deltaTime, minZoom, maxZoom);
+        bool isWithinZoomBounds = Camera.main.orthographicSize < maxZoom && Camera.main.orthographicSize > minZoom;
+
+        if (isWithinZoomBounds) {
+            // Moving camera while scrolling
+            Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float distanceMouseCenter = Vector3.Distance(mousePositionWorld, transform.position);
+            // doing distanceMouseCenter + 1 because otherwise log outputs negative values
+            float finalSideMoveSpeed = -scrollDir * Mathf.Log(distanceMouseCenter + 1) * Mathf.Log(Camera.main.orthographicSize) * sideMoveSpeed;
+            transform.position = Vector3.MoveTowards(transform.position, mousePositionWorld, finalSideMoveSpeed);
+        }
     }
 }
