8bc582fe2798d7fc376dd9e3ccb499e4f9f843c4
Jaden Balogh <jadenbalogh@gmail.com>
2020-11-14 19:28:57 -0800

    Update camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,49 +7,51 @@ public class CameraController : MonoBehaviour
     public float minZoom = 5f;
     public float maxZoom = 15f;
     [Tooltip("The scroll damp time in seconds")]
-    public float scrollDampTime = 0.8f;
+    public float scrollDampTime = 0.5f;
     public float scrollSensitivity = 1f;
-    public float scrollThreshold = 0f;
+    public float scrollThreshold = 0.1f;
 
     private float scrollVelocity = 0f;
-    private float timer;
     private float lastScrollVelocity = 0f;
-    private float interpolant = 0f;
-    private Vector2 scrollTarget;
-    private bool isScrolling;
+    private float scrollDampTimer;
+    private Vector2 lastMouseViewportPos;
+    private Vector2 lastMouseWorldPos;
 
     void Update()
     {
-        // Keep a record of the mouse position before scrolling
-        Vector2 mouseViewportCoordinates = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        Vector2 mouseWorldCoodinates = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Calculate current camera dimensions
+        Vector2 topRightWorldCoordinates = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        Vector2 bottomLeftWorldCoordinates = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        Vector2 cameraWorldDimensions = topRightWorldCoordinates - bottomLeftWorldCoordinates;
+
+        // Get mouse input
+        bool isScrolling = Mathf.Abs(Input.mouseScrollDelta.y) > scrollThreshold;
+        scrollVelocity += -Input.mouseScrollDelta.y * scrollSensitivity;
 
         // Scroll
-        isScrolling = Mathf.Abs(Input.mouseScrollDelta.y) > scrollThreshold;
-        scrollVelocity += -Input.mouseScrollDelta.y * scrollSensitivity;
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize + scrollVelocity * Time.deltaTime, minZoom, maxZoom);
 
         if (isScrolling)
         {
-            // Update values
+            // Update zoom momentum
             lastScrollVelocity = scrollVelocity;
-            timer = 0;
-            float cameraHeight = Camera.main.orthographicSize * 2;
-            Vector2 topRightWorldCoordinates = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-            Vector2 bottomLeftWorldCoordinates = Camera.main.ScreenToWorldPoint(Vector2.zero);
-            Vector2 cameraDimensionsWorld = topRightWorldCoordinates - bottomLeftWorldCoordinates;
-            scrollTarget = mouseWorldCoodinates - cameraDimensionsWorld * (mouseViewportCoordinates - Vector2.one * 0.5f);
+            scrollDampTimer = 0;
+            
+            // Update mouse position
+            lastMouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            lastMouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         else
         {
             // Update timer
-            timer += Time.deltaTime;
+            scrollDampTimer += Time.deltaTime;
 
             // Dampen scrolling
-            scrollVelocity = Mathf.Lerp(lastScrollVelocity, 0, timer / scrollDampTime);
+            scrollVelocity = Mathf.Lerp(lastScrollVelocity, 0, scrollDampTimer / scrollDampTime);
         }
 
-        // Move camera towards scrollTarget
+        // Move camera towards target offset from mouse position
+        Vector2 scrollTarget = lastMouseWorldPos - cameraWorldDimensions * (lastMouseViewportPos - Vector2.one * 0.5f);
         transform.position = new Vector3(scrollTarget.x, scrollTarget.y, -10);
     }
 }
