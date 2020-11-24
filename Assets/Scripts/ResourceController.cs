32a88528e6b1ae2d8290a7c7ae22f344d4c9e5e5
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-24 18:34:25 +0100

    Instantiate a few trees on the map

---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceController : MonoBehaviour
+{
+    public Tree treePrefab;
+
+    private List<Tree> trees;
+    void Start()
+    {
+        trees = new List<Tree>();
+        for (int i = 0; i < 10; i++)
+        {
+            trees.Add(Instantiate(treePrefab, new Vector3(10, 10, 0), Quaternion.identity));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+
+{
+    Tree(int posX, int posY)
+    {
+        transform.position = new Vector2(posX, posY);
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
