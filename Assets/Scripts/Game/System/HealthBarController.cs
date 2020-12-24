237127eb7a79e71479e5de5cdd5b703876b3576b
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-12-24 10:55:06 +0100

    Add health bar

---
+++ Assets/Scripts/Game/System/HealthBarController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private Image HealthBarImage;
+
+    public void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if(HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if(HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    public void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+    }
+}
