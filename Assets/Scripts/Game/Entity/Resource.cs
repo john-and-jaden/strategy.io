8e31ebe38f3db2079ddfd067d0e7678c353cc85a
Jaden Balogh <jadenbalogh@gmail.com>
2020-12-29 15:55:07 -0800

    Add resource drop pop effect

--- Assets/Scripts/Game/Entity/Resource.cs
+++ Assets/Scripts/Game/Entity/Resource.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class Resource : Selectable
 {
+    [SerializeField] private ResourceDrop resourceDropPrefab;
+    [SerializeField] private int resourceDropCount = 2;
+    [SerializeField] private float resourceDropMaxPopForce = 5f;
+
     private Cluster cluster;
     public Cluster Cluster
     {
@@ -10,14 +14,24 @@ public class Resource : Selectable
         set { cluster = value; }
     }
 
-    [SerializeField] private ResourceDrop resourceDropPrefab;
-
     override protected void DestroySelf()
     {
         DestroyIndicators();
         cluster.Resources.Remove(this);
         onDestroyed.Invoke();
-        Instantiate(resourceDropPrefab, transform.position, Quaternion.identity);
+        SpawnResourceDrops();
         Destroy(gameObject);
+    }
+
+    private void SpawnResourceDrops()
+    {
+        for (int i = 0; i < resourceDropCount; i++)
+        {
+            ResourceDrop drop = Instantiate(resourceDropPrefab, transform.position, Quaternion.identity);
+            Vector2 forceDir = Random.insideUnitCircle.normalized;
+            float forceScale = Random.Range(0, 1f);
+            Vector2 popForce = forceDir * forceScale * resourceDropMaxPopForce;
+            drop.GetComponent<Rigidbody2D>().AddForce(popForce, ForceMode2D.Impulse);
+        }
     }
 }
