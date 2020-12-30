710eb456ad9c6a72ee937d51f8b8e3f80385fe4c
Jaden Balogh <jadenbalogh@gmail.com>
2020-12-29 19:40:45 -0800

    Add drop magnetism and resource updating

--- Assets/Scripts/Game/Entity/ResourceDrop.cs
+++ Assets/Scripts/Game/Entity/ResourceDrop.cs
@@ -1,7 +1,67 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceDrop : MonoBehaviour
+public abstract class ResourceDrop : MonoBehaviour
 {
-    
+    private const int MAX_ATTRACTION_TARGETS = 10;
+    private const float DESTROY_DIST_SQR = 0.1f;
+
+    [SerializeField] protected int resourceAmount = 1;
+    [SerializeField] private float attractionRadius = 1f;
+    [SerializeField] private float attractionForce = 1f;
+    [SerializeField] private LayerMask attractionMask;
+
+    private Collider2D[] attractionTargets = new Collider2D[MAX_ATTRACTION_TARGETS];
+
+    private new Rigidbody2D rigidbody2D;
+
+    protected void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected void FixedUpdate()
+    {
+        Worker nearestWorker = GetNearestWorker();
+        if (nearestWorker != null)
+        {
+            // Get vector towards the nearest worker
+            Vector2 towardsWorker = nearestWorker.transform.position - transform.position;
+
+            // If we are close enough, collect this drop
+            float workerDistSqr = towardsWorker.sqrMagnitude;
+            if (workerDistSqr < DESTROY_DIST_SQR)
+            {
+                Collect();
+                Destroy(gameObject);
+                return;
+            }
+
+            // Magnetize towards nearest worker
+            Vector2 forceDir = towardsWorker.normalized;
+            rigidbody2D.AddForce(forceDir * attractionForce, ForceMode2D.Impulse);
+        }
+    }
+
+    protected abstract void Collect();
+
+    private Worker GetNearestWorker()
+    {
+        Worker nearestWorker = null;
+        float shortestDistSqr = float.MaxValue;
+        int workerCount = Physics2D.OverlapCircleNonAlloc(transform.position, attractionRadius, attractionTargets, attractionMask);
+        for (int i = 0; i < workerCount; i++)
+        {
+            if (attractionTargets[i].TryGetComponent<Worker>(out Worker worker))
+            {
+                float workerDistSqr = (worker.transform.position - transform.position).sqrMagnitude;
+                if (workerDistSqr < shortestDistSqr)
+                {
+                    nearestWorker = worker;
+                    shortestDistSqr = workerDistSqr;
+                }
+            }
+        }
+        return nearestWorker;
+    }
 }
---
+++ Assets/Scripts/Game/Entity/TreeDrop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDrop : ResourceDrop
+{
+    protected override void Collect()
+    {
+        GameManager.ResourceSystem.AddWood(resourceAmount);
+    }
+}
