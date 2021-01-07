5b931b8345b968f829c8021a25ddb8585c344082
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-01-07 18:16:34 +0100

    Add basic XP display

---
+++ Assets/Scripts/UI/XPDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class XPDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI XPText;
+
+    void Start()
+    {
+        GameManager.ResourceSystem.AddWoodChangedListener(SetXPText);
+    }
+
+    private void SetXPText(int XP)
+    {
+        XPText.text = XP.ToString();
+    }
+}
